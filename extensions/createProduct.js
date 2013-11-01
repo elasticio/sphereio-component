de8c7395f8501e30892eac1c327e429572f934c0
Igor Drobiazko <igor.drobiazko@gmail.com>
2013-11-01 13:02:04 +0100

    Logs

--- extensions/createProduct.js
+++ extensions/createProduct.js
@@ -1,11 +1,17 @@
 exports.preProcess = function (msg, cfg, snapshot, cb) {
 
+    console.log("preProcess createProduct");
+
+    console.log(msg.body);
+
     var id = msg.body.productType;
 
     msg.body.productType = {
         type: "product-type",
         id: id
     };
+
+    console.log(msg.body);
 
     cb();
 };
