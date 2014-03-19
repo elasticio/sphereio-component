fbf6de156d769cf34124cdafa406d785cbbe01e4
Nenad Nikolic <iznenad.nikolic@gmail.com>
2014-03-19 02:14:10 +0100

    Refactoring getMetaModel, providing a test suite

--- extensions/sphereio.js
+++ extensions/sphereio.js
@@ -1,8 +1,12 @@
 var util = require("util");
 var moment = require("moment");
 var request = require('request');
-var fs = require('fs');
 var _ = require("underscore");
+var Q = require('q');
+var fs = require('fs');
+var path = require('path');
+var metaModelDataCollector = require('./helpers/metaModelDataCollector');
+var metaModelDataProcessor = require('./helpers/metaModelDataProcessor');
 
 exports.setWhereLastModifiedGreaterThan = function (msg, cfg, snapshot, cb) {
 
@@ -44,147 +48,37 @@ exports.updateSnapshotWithLastModified = function (msg, snapshot, cb) {
     cb(null, msg, snapshot);
 };
 
-var updateSnapshot = function(data, snapshot) {
+var updateSnapshot = function (data, snapshot) {
     var lastModifiedAt = data.lastModifiedAt;
 
     var maxLastModifiedAt = snapshot.lastModifiedAt || "1970-01-01T00:00:00.000Z";
 
-    if(moment(lastModifiedAt).isAfter(maxLastModifiedAt)) {
+    if (moment(lastModifiedAt).isAfter(maxLastModifiedAt)) {
 
         snapshot.lastModifiedAt = lastModifiedAt;
     }
 };
 
 exports.getMetaModel = function (options, callback) {
-
-    exports.createMetaModelRequirements(options.cfg, options.resourceName, function (err, resource, languages) {
-
-        if (err) {
-            return callback(err);
-        }
-
-        var languagesMetaData = createLanguagesMetaData(languages);
-
-        var model = _.find(resource.models, function (model) {
-            return model.id.toLowerCase() === options.modelName;
-        });
-
-        var outMeta = {
-            "type": "object",
-            "properties": model.properties
-        };
-
-        //perform deep copy
-        var inMeta = JSON.parse(JSON.stringify(options.availableMetadata.in));
-
-        injectLocalizedStrings(outMeta, languagesMetaData);
-        injectLocalizedStrings(inMeta, languagesMetaData);
-
-        var metaData = {
-            "in": inMeta,
-            "out": outMeta
-        }
-
-        callback(null, metaData);
-    });
-}
-
-var injectLocalizedStrings = function (model, languageMeta) {
-
-    _.each(model.properties, function (property, propertyName) {
-
-        if (property.type === "lstring") {
-
-            var title = property.title || (_.first(propertyName).toUpperCase() + _.rest(propertyName).join(''));
-
-            //deep copy of language meta data as we will manipulate it's properties
-            var languageMetaCopy = JSON.parse(JSON.stringify(languageMeta));
-
-            _.each(languageMetaCopy, function (languageCodeProperties, languageCode) {
-                languageCodeProperties.title = title + " (" + languageCode + ")";
-            });
-
-            property.type = "object";
-            property.properties = languageMetaCopy;
-
-            return;
-        }
-
-        if (property.properties) injectLocalizedStrings(property, languageMeta);
-    });
-};
-
-var createLanguagesMetaData = function getLanguagesMetaData(arrayOfLanguages) {
-    return arrayOfLanguages.reduce(function (previousResult, language) {
-
-        previousResult[language] = {type: "string"};
-
-        return previousResult;
-
-    }, Object.create(Object.prototype));
-};
-
-exports.createMetaModelRequirements = function (cfg, resourceName, cb) {
-    exports.attachTokenTo(cfg,
-
-        function (err) {
-
-            if (err) {
-                console.log(err);
-                return cb(err);
-            }
-
-            fs.readFile(__dirname + '/../api-docs.json', function (err, apiDocsContent) {
-
-                if (err) {
-                    console.log(err);
-                    return cb(err);
-                }
-
-                fs.readFile(__dirname + '/../' + resourceName + '.json', function (err, resourceContent) {
-                    if (err) {
-                        console.log(err);
-                        return cb(err);
-                    }
-                    var apiDocs = JSON.parse(apiDocsContent);
-                    var resource = JSON.parse(resourceContent);
-
-                    var projectUri = apiDocs.basePath + cfg.project;
-
-                    request.get({
-                        uri: projectUri,
-                        headers: {
-                            "Authorization": "Bearer " + cfg.oauth.access_token
-                        }
-                    }, function (err, response, body) {
-                        if (err) {
-                            console.log(err);
-                            return cb(err);
-                        }
-
-                        if (response.statusCode != 200) {
-                            console.log(body);
-                            return cb(new Error(body));
-                        }
-                        console.log(typeof body);
-                        console.log(JSON.stringify(body));
-                        var languages = JSON.parse(body).languages;
-
-                        cb(null, resource, languages);
-                    });
-                });
-            });
-        });
+    Q.nfcall(exports.attachTokenTo, options.cfg).then(function () {
+        return options;
+    })
+        .then(metaModelDataCollector.getData)
+        .spread(metaModelDataProcessor.processData)
+        .then(function (metaModel) {
+            callback(null, metaModel);
+        }).fail(function (err) {
+            callback(err);
+        }).done();
 }
 
 const OAUTH_URL = 'https://auth-v0.sphere.io/oauth/token';
 
 exports.attachTokenTo = function (cfg, cb) {
-    var clientId = cfg.client;
 
     var uri = require('url').parse(OAUTH_URL);
 
-    uri.auth = [clientId, cfg.clientSecret].join(':');
+    uri.auth = [cfg.client, cfg.clientSecret].join(':');
 
     request.post({
         uri: uri,
@@ -206,7 +100,7 @@ exports.attachTokenTo = function (cfg, cb) {
 
         var payload = JSON.parse(body);
 
-        console.log("Received new access token for client: %s", clientId);
+        console.log("Received new access token for client: %s", cfg.client);
 
         cfg.oauth = {
             "access_token": payload["access_token"]
---
+++ spec/sphereio.spec.js
@@ -0,0 +1,166 @@
+var sphereio = require('../extensions/sphereio');
+var nock = require('nock');
+var url = require('url');
+var metaModelDataCollector = require('../extensions/helpers/metaModelDataCollector');
+var metaModelDataProcessor = require('../extensions/helpers/metaModelDataProcessor');
+
+var Q = require('q');
+
+describe('SphereIO helpers', function () {
+    xdescribe('attachTokenTo', function () {
+
+        var interceptor;
+        var cfg;
+
+        it('should callback with an error if one occured', function () {
+            interceptor = nock('https://auth-v0.sphere.io')
+                .post("/oauth/token", "grant_type=client_credentials&scope=manage_project%3AaSphereioProject");
+
+            cfg = {
+                client: "someClient",
+                clientSecret: "verySecretStuff",
+                project: "aSphereioProject"
+            };
+            interceptor.reply(400, "ok");
+
+            sphereio.attachTokenTo(cfg, function (err) {
+                expect(err).toBeDefined();
+                expect(cfg.ouath).toBeUndefined();
+            });
+        });
+
+        it('should attach a token to cfg object', function () {
+            interceptor = nock('https://auth-v0.sphere.io')
+                .post("/oauth/token", "grant_type=client_credentials&scope=manage_project%3AaSphereioProject");
+
+            cfg = {
+                client: "someClient",
+                clientSecret: "verySecretStuff",
+                project: "aSphereioProject"
+            };
+
+            interceptor.reply(200, {
+                "access_token": "aToken"
+            });
+
+
+            var error;
+            runs(function () {
+                sphereio.attachTokenTo(cfg, function () {
+                })
+            });
+
+            waitsFor(function () {
+                return cfg.oauth;
+            })
+
+            runs(function () {
+                expect(cfg.oauth).toBeDefined()
+            });
+        });
+
+    });
+
+    describe('getMetaModel', function () {
+
+        it('should callback with the processed metaModel', function () {
+
+            var options = {
+                cfg: {
+                    client: "someClient",
+                    clientSecret: "verySecretStuff",
+                    project: "aSphereioProject"
+                },
+                resourceName: 'products',
+                modelName: 'product'
+            };
+
+            spyOn(sphereio, 'attachTokenTo').andCallFake(function (err, callback) {
+                console.log(arguments);
+
+                callback();
+            });
+
+            spyOn(metaModelDataCollector, 'getData').andCallFake(function (result) {
+
+                console.log(arguments);
+                return Q.fcall(function () {
+                    return [
+                        {
+                            "out": {
+                                "type:": "object",
+                                properties: {
+                                    "name": {
+                                        type: "string"
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            "en": {
+                                type: "string"
+                            }
+                        }
+                    ];
+                });
+            });
+
+            spyOn(metaModelDataProcessor, 'processData').andCallFake(function (metadata, languageMeta) {
+                return Q.fcall(function () {
+                    return metadata;
+                });
+            });
+
+            sphereio.getMetaModel(options, function (err, result) {
+                expect(result).toEqual(result);
+            });
+        });
+
+        it('should handle callback with an error if it occurs somewhere in the promise chain', function () {
+            var options = {
+                cfg: {
+                    client: "someClient",
+                    clientSecret: "verySecretStuff",
+                    project: "aSphereioProject"
+                },
+                resourceName: 'products',
+                modelName: 'product'
+            };
+
+            spyOn(sphereio, 'attachTokenTo').andCallFake(function (err, callback) {
+                console.log(arguments);
+
+                callback();
+            });
+
+            spyOn(metaModelDataCollector, 'getData').andCallFake(function (result) {
+                Q.fcall(function () {
+                    throw error;
+                });
+            });
+
+            spyOn(metaModelDataProcessor, 'processData').andCallFake(function (metadata, languageMeta) {
+                return Q.fcall(function () {
+                    return metadata;
+                });
+            });
+
+            var error;
+            runs(function () {
+                sphereio.getMetaModel(options, function (err, result) {
+                    error = err;
+                });
+            });
+
+            waitsFor(function () {
+                return error;
+            });
+
+            runs(function () {
+                expect(error).toBeDefined();
+            });
+        });
+    });
+
+})
+;
