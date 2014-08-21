f3c898e458d642f92618969d6b308d1123250f3e
Igor Drobiazko <igor@elastic.io>
2014-08-21 22:02:34 +0200

    Added coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,70 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        clean: {
-            all: ["coverage"]
-        },
         "jasmine_node": {
-            projectRoot: ".",
-            requirejs: false,
-            forceExit: true
+            options: {
+                forceExit: true,
+                extensions: 'js'
+            },
+            spec: ["./spec"],
+            instrumented: ["./coverage/instrument/spec"]
         },
         jshint: {
             files: ['Gruntfile.js', 'spec/**/*.js']
+        },
+        // start - code coverage settings
+
+        env: {
+            coverage: {
+                APP_DIR_FOR_CODE_COVERAGE: '../coverage/instrument/lib/'
+            }
+        },
+
+
+        clean: {
+            coverage: {
+                src: ['coverage/']
+            }
+        },
+
+
+        instrument: {
+            files: ['lib/*.js', 'spec/*.js'],
+            options: {
+                lazy: true,
+                basePath: 'coverage/instrument/'
+            }
+        },
+
+
+        storeCoverage: {
+            options: {
+                dir: 'coverage/reports'
+            }
+        },
+
+
+        makeReport: {
+            src: 'coverage/reports/**/*.json',
+            options: {
+                type: 'lcov',
+                dir: 'coverage/reports',
+                print: 'detail'
+            }
+        },
+
+        // end - code coverage settings
+
+        coveralls: {
+            test: {
+                // LCOV coverage file relevant to every target
+                src: 'coverage/reports/lcov.info',
+
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            }
         }
     });
 
@@ -21,10 +75,15 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-istanbul');
+
+    grunt.loadNpmTasks('grunt-env');
+
+    grunt.loadNpmTasks('grunt-coveralls');
 
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'jshint', 'jasmine_node']);
+    grunt.registerTask('default', ['clean', 'jshint', 'jasmine_node:spec']);
 
-    grunt.registerTask('coverage', ['clean', 'cover-specs']);
+    grunt.registerTask('coverage', ['clean', 'env:coverage',
+        'instrument', 'jasmine_node:instrumented', 'storeCoverage', 'makeReport', 'coveralls:test']);
 };
