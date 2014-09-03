b8ee083f80bde0b97478cec0b2e76a92e219cc9f
ellenaua <elenasharovar@gmail.com>
2014-09-03 12:54:56 +0300

    Finished QueryOrders.js + tests

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -1,4 +1,4 @@
-describe('Sphere.io queryCustomers.js', function () {
+describe('Sphere.io helpers', function () {
     var helpers = require('../lib/helpers.js');
     var allCustomers = require('./data/all_customers.json.js');
     var allOrders = require('./data/all_orders.json.js');
@@ -30,5 +30,62 @@ describe('Sphere.io queryCustomers.js', function () {
         var snapshot = {};
         helpers.updateSnapshotWithLastModified(allOrders.results, snapshot);
         expect(snapshot.lastModifiedAt).toEqual(allOrders.results[1].lastModifiedAt);
+    });
+
+
+    describe('Sphere.io helpers convertLStrings', function () {
+
+        var input = {
+            "type": "object",
+            "properties": {
+                "offset": {
+                    "type": "number",
+                    "title": "Offset",
+                    "required": true
+                },
+                "total": {
+                    "type": "number",
+                    "title": "Total",
+                    "required": true
+                },
+                "results": {
+                    "type": "array",
+                    "required": true,
+                    "properties": {
+                        "id": {
+                            "title": "ID",
+                            "type": "string",
+                            "required": true
+                        },
+                        "name": {
+                            "title": "Name",
+                            "type": "lstring",
+                            "required": true
+                        }
+                    }
+                }
+            }
+        };
+
+        var nameConverted = {
+            type : 'object',
+            properties : {
+                en : {
+                    title : 'Name (en)',
+                    type : 'string',
+                    required : true
+                },
+                de : {
+                    title : 'Name (de)',
+                    type : 'string',
+                    required : true
+                }
+            }
+        };
+
+        it('Should convert lstring property to array of string properties', function () {
+            var result = helpers.convertLStrings(input, ['de', 'en']);
+            expect(result.properties.results.properties.name).toEqual(nameConverted);
+        });
     });
 });
