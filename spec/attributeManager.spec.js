f991bd839fb26fa281bf52e8b361182d5c718e0e
ellenaua <elenasharovar@gmail.com>
2014-09-05 14:07:41 +0300

    Bugfix: readAttributes should return undefined if no data provided

--- spec/attributeManager.spec.js
+++ spec/attributeManager.spec.js
@@ -81,15 +81,22 @@ describe('readAttributes', function () {
         expect(result).toEqual(expected);
     });
 
-    it('Should return empty array if message is undefined', function(){
-        var result = attributeManager.readAttributes(data, undefined);
-        expect(result).toEqual([]);
+    it('Should return undefined if message is null', function(){
+        var result = attributeManager.readAttributes(null, {});
+        expect(result).toEqual(undefined);
     });
 
-    it('Should return empty array if message body is empty', function(){
-        var result = attributeManager.readAttributes(data, {});
-        expect(result).toEqual([]);
+    it('Should return undefined if message is undefined', function(){
+        var result = attributeManager.readAttributes(undefined, {});
+        expect(result).toEqual(undefined);
     });
+
+    it('Should return undefined if productTypeAttributes are undefined', function(){
+        var result = attributeManager.readAttributes(data, undefined);
+        expect(result).toEqual(undefined);
+    });
+
+
 });
 
 describe('describeAttributes', function () {
@@ -166,5 +173,165 @@ describe('describeAttributes', function () {
     it('Should describe attributes', function(){
         var result = attributeManager.describeAttributes(attributes, 'Master Variant');
         expect(result).toEqual(description);
+    });
+});
+
+describe('readProduct', function () {
+
+    it('Should read product in correct way', function(){
+
+        var msg = {
+            body: {
+                name: {
+                    en: 'Product Name'
+                },
+                slug:  {
+                    en: 'Product Slug'
+                },
+                description: {
+                    en: 'Product Description'
+                },
+                categories: '',
+                metaTitle: {
+                    en: 'Product metaTitle'
+                },
+                metaDescription: {
+                    en: 'Product metaDescription'
+                },
+                metaKeywords: {
+                    en: 'Product metaKeywords'
+                },
+                masterVariant: {
+                    sku: 'master-variant-sku',
+                    prices: [{
+                        value: {
+                            'currencyCode': 'EUR',
+                            'centAmount': '1200'
+                        }
+                    }],
+                    attributes: {
+                        attribute1: 'Lorem Ipsum',
+                        attribute2: '500',
+                        attribute3: {
+                            en: 'English',
+                            de: 'Deutsch'
+                        }
+                    }
+                },
+                variants: [{
+                    sku: 'variant-sku',
+                    prices: [{
+                        value: {
+                            'currencyCode': 'EUR',
+                            'centAmount': '8000'
+                        }
+                    }],
+                    attributes: {
+                        attribute1: 'Lorem Ipsum',
+                        attribute2: '700'
+                    }
+                }]
+            }
+        };
+
+        var expectedProduct = {
+            "name": {
+                "en": "Product Name"
+            },
+            "productType": {
+                "typeId": "product-type",
+                "id": "product-type-id"
+            },
+            "slug": {
+                "en": "Product Slug"
+            },
+            "description": {
+                "en": "Product Description"
+            },
+            "metaTitle": {
+                "en": "Product metaTitle"
+            },
+            "metaDescription": {
+                "en": "Product metaDescription"
+            },
+            "metaKeywords": {
+                "en": "Product metaKeywords"
+            },
+            "masterVariant": {
+                "sku": "master-variant-sku",
+                "prices": [
+                    {
+                        "value": {
+                            "currencyCode": "EUR",
+                            "centAmount": 1200
+                        }
+                    }
+                ],
+                "attributes": [
+                    {
+                        "name": "attribute1",
+                        "value": "Lorem Ipsum"
+                    },
+                    {
+                        "name": "attribute2",
+                        "value": 500
+                    },
+                    {
+                        "name": "attribute3",
+                        "value": {
+                            "en": "English",
+                            "de": "Deutsch"
+                        }
+                    }
+                ]
+            },
+            "variants": [
+                {
+                    "sku": "variant-sku",
+                    "prices": [
+                        {
+                            "value": {
+                                "currencyCode": "EUR",
+                                "centAmount": 8000
+                            }
+                        }
+                    ],
+                    "attributes": [
+                        {
+                            "name": "attribute1",
+                            "value": "Lorem Ipsum"
+                        },
+                        {
+                            "name": "attribute2",
+                            "value": 700
+                        }
+                    ]
+                }
+            ]
+        };
+
+        var cfg = {
+            productType: 'product-type-id'
+        };
+
+        var attributeDescriptions = [
+            {'name': 'attribute1', 'type': {'name': 'text'}},
+            {'name': 'attribute2', 'type': {'name': 'number'}},
+            {'name': 'attribute3', 'type': {'name': 'ltext'}}
+        ];
+
+        var result = attributeManager.readProduct(cfg, msg, attributeDescriptions);
+        expect(result).toEqual(expectedProduct);
+    });
+
+    it('Should throw an error if readProduct failed', function(){
+
+        var msg = {};
+        var cfg = {};
+        var attributeDescriptions = [];
+
+        expect(function(){
+            attributeManager.readProduct(cfg, msg, attributeDescriptions);
+        }).toThrow();
     });
 });
