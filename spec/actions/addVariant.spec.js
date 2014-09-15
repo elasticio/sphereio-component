4f9946691147d880c3e7b3fab29c41b86095e412
Nenad Nikolic <iznenad.nikolic@gmail.com>
2014-09-15 16:14:38 +0200

    Fixes #740

--- spec/actions/addVariant.spec.js
+++ spec/actions/addVariant.spec.js
@@ -2,6 +2,7 @@ describe('Add Variant', function() {
 
     var addVariant = require('../../lib/actions/addVariant');
     var nock = require('nock');
+    var responseData = require('../data/add_variant_data.json');
 
     var cfg = {
         client: '1',
@@ -72,7 +73,7 @@ describe('Add Variant', function() {
                 });
 
             nock('https://api.sphere.io:443')
-                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3DaMasterVariantReference)))')
+                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3D%22aMasterVariantReference%22)))')
                 .reply(200, {
                     "statusCode": 200,
                     "body": {
@@ -111,69 +112,6 @@ describe('Add Variant', function() {
 
     });
 
-    var response = {
-        "statusCode": 200,
-        "body": {
-            "offset": 0,
-            "count": 1,
-            "total": 1,
-            "results": [{
-                "id": "anId",
-                "version": 3,
-                "productType": {
-                    "typeId": "product-type",
-                    "id": "aProductType"
-                },
-                "catalogs": [],
-                "masterData": {
-                    "current": {
-                        "name": {
-                            "en": "Live-test-name-11111111111"
-                        },
-                        "categories": [],
-                        "slug": {
-                            "en": "Live-test-slug-11111111111"
-                        },
-                        "masterVariant": {
-                            "id": 1,
-                            "sku": "aMasterVariantReference",
-                            "prices": [],
-                            "images": [],
-                            "attributes": []
-                        },
-                        "variants": []
-                    },
-                    "staged": {
-                        "name": {
-                            "en": "Live-test-name-11111111111"
-                        },
-                        "categories": [],
-                        "slug": {
-                            "en": "Live-test-slug-11111111111"
-                        },
-                        "masterVariant": {
-                            "id": 1,
-                            "sku": "aMasterVariantReference",
-                            "prices": [],
-                            "images": [],
-                            "attributes": [{
-                                "name": "textattr",
-                                "value": "Nenad"
-                            }]
-                        },
-                        "variants": []
-                    },
-                    "published": false,
-                    "hasStagedChanges": true
-                },
-                "catalogData": {},
-                "lastVariantId": 1,
-                "createdAt": "2014-09-10T13:43:49.400Z",
-                "lastModifiedAt": "2014-09-15T12:38:20.632Z"
-            }]
-        }
-    };
-
     describe('when a product update fails for whatever reason', function() {
         beforeEach(function() {
 
@@ -186,8 +124,8 @@ describe('Add Variant', function() {
                 });
 
             nock('https://api.sphere.io:443')
-                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3DaMasterVariantReference)))')
-                .reply(200, response);
+                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3D%22aMasterVariantReference%22)))')
+                .reply(200, responseData.queryProductResponse);
 
 
             nock('https://api.sphere.io:443')
@@ -234,36 +172,13 @@ describe('Add Variant', function() {
                 });
 
             nock('https://api.sphere.io:443')
-                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3DaMasterVariantReference)))')
-                .reply(200, response);
+                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3D%22aMasterVariantReference%22)))')
+                .reply(200, responseData.queryProductResponse);
 
 
             nock('https://api.sphere.io:443')
                 .post('/elasticio/products/anId', [])
-                .reply(409, {
-                    "statusCode": 409,
-                    "message": "A duplicate combination of the variant values (sku, images, prices, attributes) exists.",
-                    "errors": [{
-                        "code": "DuplicateVariantValues",
-                        "message": "A duplicate combination of the variant values (sku, images, prices, attributes) exists.",
-                        "variantValues": {
-                            "sku": "anSku",
-                            "prices": [],
-                            "images": [],
-                            "attributes": []
-                        }
-                    }],
-                    "originalRequest": {
-                        "endpoint": "/products/33afbe8a-fd02-4b4f-80ac-66ff05dbaed3",
-                        "payload": {
-                            "version": 10,
-                            "actions": [{
-                                "action": "addVariant",
-                                "sku": "anSku"
-                            }]
-                        }
-                    }
-                });
+                .reply(409, responseData.reboundResponse);
         });
 
         it('should emit rebound', function() {
@@ -293,105 +208,6 @@ describe('Add Variant', function() {
     });
     describe('success case', function() {
 
-        var addVariantResponse = {
-            "statusCode": 200,
-            "body": {
-                "id": "33afbe8a-fd02-4b4f-80ac-66ff05dbaed3",
-                "version": 5,
-                "productType": {
-                    "typeId": "product-type",
-                    "id": "5dfdc7a9-76a7-4e02-8c4e-6b5b9bd58ef8"
-                },
-                "catalogs": [],
-                "masterData": {
-                    "current": {
-                        "name": {
-                            "en": "WB ATHLETIC TANK"
-                        },
-                        "description": {
-                            "en": "Sample description"
-                        },
-                        "categories": [{
-                            "typeId": "category",
-                            "id": "9ac573d5-8f0b-4451-aa57-7626f264aaef"
-                        }],
-                        "slug": {
-                            "en": "wb-athletic-tank1368609991639"
-                        },
-                        "masterVariant": {
-                            "id": 1,
-                            "sku": "sku_WB_ATHLETIC_TANK_variant1_1368609991639",
-                            "prices": [{
-                                "value": {
-                                    "currencyCode": "EUR",
-                                    "centAmount": 8400
-                                }
-                            }],
-                            "images": [{
-                                "url": "https://www.commercetools.com/cli/data/253265444_1.jpg",
-                                "dimensions": {
-                                    "w": 1400,
-                                    "h": 1400
-                                }
-                            }],
-                            "attributes": []
-                        },
-                        "variants": []
-                    },
-                    "staged": {
-                        "name": {
-                            "en": "WB ATHLETIC TANK"
-                        },
-                        "description": {
-                            "en": "Sample description"
-                        },
-                        "categories": [{
-                            "typeId": "category",
-                            "id": "9ac573d5-8f0b-4451-aa57-7626f264aaef"
-                        }],
-                        "slug": {
-                            "en": "wb-athletic-tank1368609991639"
-                        },
-                        "masterVariant": {
-                            "id": 1,
-                            "sku": "sku_WB_ATHLETIC_TANK_variant1_1368609991639",
-                            "prices": [{
-                                "value": {
-                                    "currencyCode": "EUR",
-                                    "centAmount": 50
-                                }
-                            }],
-                            "images": [{
-                                "url": "https://www.commercetools.com/cli/data/253265444_1.jpg",
-                                "dimensions": {
-                                    "w": 1400,
-                                    "h": 1400
-                                }
-                            }],
-                            "attributes": []
-                        },
-                        "variants": [{
-                            "id": 2,
-                            "sku": "anSku",
-                            "prices": [],
-                            "images": [],
-                            "attributes": []
-                        }]
-                    },
-                    "published": true,
-                    "hasStagedChanges": true
-                },
-                "catalogData": {},
-                "taxCategory": {
-                    "typeId": "tax-category",
-                    "id": "36ce04f7-6e1c-4115-be37-b14abb8eff3e"
-                },
-                "lastVariantId": 2,
-                "createdAt": "1970-01-01T00:00:00.001Z",
-                "lastModifiedAt": "2014-09-15T13:20:13.891Z"
-            }
-        };
-
         beforeEach(function() {
 
             nock('https://auth.sphere.io').post('/oauth/token')
@@ -403,13 +219,13 @@ describe('Add Variant', function() {
                 });
 
             nock('https://api.sphere.io:443')
-                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3DaMasterVariantReference)))')
-                .reply(200, response);
+                .get('/elasticio/products?where=masterData(current(masterVariant(sku%3D%22aMasterVariantReference%22)))')
+                .reply(200, responseData.queryProductResponse);
 
 
             nock('https://api.sphere.io:443')
                 .post('/elasticio/products/anId', [])
-                .reply(200, addVariantResponse);
+                .reply(200, responseData.addVariantResponse);
         });
 
         it('should emit the response of product update', function() {
