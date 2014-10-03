0f9baff39e452726e53514bd1df3099d0d2cd98e
Elena Sharovar <elenasharovar@gmail.com>
2014-10-03 13:37:59 +0300

    Amount and centAmount functions. Refs issue #101

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -3,37 +3,40 @@ describe('Sphere.io helpers', function () {
     var allCustomers = require('./data/all_customers.json.js');
     var allOrders = require('./data/all_orders.json.js');
 
-    it('Should update snapshot field `lastModifiedAt` if this field is empty', function () {
-        var snapshot = {};
-        helpers.updateSnapshotWithLastModified(allCustomers.results, snapshot);
-        expect(snapshot.lastModifiedAt).toEqual('2014-08-19T00:00:00.001Z');
+    describe('Sphere.io helpers: updateSnapshotWithLastModified', function () {
+
+        it('Should update snapshot field `lastModifiedAt` if this field is empty', function () {
+            var snapshot = {};
+            helpers.updateSnapshotWithLastModified(allCustomers.results, snapshot);
+            expect(snapshot.lastModifiedAt).toEqual('2014-08-19T00:00:00.001Z');
+        });
+
+        it('Should update snapshot field `lastModifiedAt` if snapshot\'s field in the past', function () {
+            var snapshot = {
+                lastModifiedAt: '2014-08-18T00:00:00.001Z'
+            };
+            helpers.updateSnapshotWithLastModified(allCustomers.results, snapshot);
+            expect(snapshot.lastModifiedAt).toEqual('2014-08-19T00:00:00.001Z');
+        });
+
+        it('Should not update snapshot field `lastModifiedAt` if snapshot\'s field in the future', function () {
+            var future = '2014-08-22T00:00:00.001Z';
+            var snapshot = {
+                lastModifiedAt: future
+            };
+            helpers.updateSnapshotWithLastModified(allCustomers.results, snapshot);
+            expect(snapshot.lastModifiedAt).toEqual(future);
+        });
+
+        it('Should update snapshot field `lastModifiedAt` if this field is empty', function () {
+            var snapshot = {};
+            helpers.updateSnapshotWithLastModified(allOrders.results, snapshot);
+            expect(snapshot.lastModifiedAt).toEqual(allOrders.results[1].lastModifiedAt);
+        });
+
     });
 
-    it('Should update snapshot field `lastModifiedAt` if snapshot\'s field in the past', function () {
-        var snapshot = {
-            lastModifiedAt: '2014-08-18T00:00:00.001Z'
-        };
-        helpers.updateSnapshotWithLastModified(allCustomers.results, snapshot);
-        expect(snapshot.lastModifiedAt).toEqual('2014-08-19T00:00:00.001Z');
-    });
-
-    it('Should not update snapshot field `lastModifiedAt` if snapshot\'s field in the future', function () {
-        var future = '2014-08-22T00:00:00.001Z';
-        var snapshot = {
-            lastModifiedAt: future
-        };
-        helpers.updateSnapshotWithLastModified(allCustomers.results, snapshot);
-        expect(snapshot.lastModifiedAt).toEqual(future);
-    });
-
-    it('Should update snapshot field `lastModifiedAt` if this field is empty', function () {
-        var snapshot = {};
-        helpers.updateSnapshotWithLastModified(allOrders.results, snapshot);
-        expect(snapshot.lastModifiedAt).toEqual(allOrders.results[1].lastModifiedAt);
-    });
-
-
-    describe('Sphere.io helpers convertLStrings', function () {
+    describe('Sphere.io helpers: convertLStrings', function () {
 
         var input = {
             'type': 'object',
@@ -88,4 +91,117 @@ describe('Sphere.io helpers', function () {
             expect(result.properties.results.properties.name).toEqual(nameConverted);
         });
     });
+
+
+    describe('Sphere.io helpers: centAmountFromAmount', function () {
+
+        it('Should convert to centAmount strings with .', function () {
+            var result = helpers.centAmountFromAmount("12.35");
+            expect(result).toEqual(1235);
+        });
+
+        it('Should convert to centAmount numbers', function () {
+            var result = helpers.centAmountFromAmount(12.35);
+            expect(result).toEqual(1235);
+        });
+
+        it('Should raise exception if string is empty', function () {
+            expect(function(){
+                helpers.centAmountFromAmount("");
+            }).toThrow('"" is not valid amount value');
+        });
+
+        it('Should raise exception if string is a number with ,', function () {
+            expect(function(){
+                helpers.centAmountFromAmount("12,35");
+            }).toThrow('"12,35" is not valid amount value');
+        });
+
+        it('Should raise exception if string is a number with , and .', function () {
+            expect(function(){
+                helpers.centAmountFromAmount("12,352.66");
+            }).toThrow('"12,352.66" is not valid amount value');
+        });
+    });
+
+    describe('Sphere.io helpers: amountFromCentAmount', function () {
+
+        it('Should convert number in a correct way', function () {
+            var result = helpers.amountFromCentAmount(1200);
+            expect(result).toEqual(12.00);
+        });
+
+        it('Should convert string in a correct way', function () {
+            var result = helpers.amountFromCentAmount('1234');
+            expect(result).toEqual(12.34);
+        });
+
+        it('Should raise exception if centAmount is floating point number', function () {
+            expect(function(){
+                helpers.amountFromCentAmount(12.35);
+            }).toThrow('"12.35" is not valid centAmount value');
+        });
+
+        it('Should raise exception if centAmount is a string with ,', function () {
+            expect(function(){
+                helpers.amountFromCentAmount("12,35");
+            }).toThrow('"12,35" is not valid centAmount value');
+        });
+
+        it('Should raise exception if centAmount is a string with .', function () {
+            expect(function(){
+                helpers.amountFromCentAmount("12.35");
+            }).toThrow('"12.35" is not valid centAmount value');
+        });
+    });
+
+    describe('Sphere.io helpers: centAmountsToAmounts and amountsToCentAmounts', function () {
+
+        var objWithCentAmounts = {
+            name: 'Jessica',
+            surname: 'Simpson',
+            cartItems: [{
+                name: 'Book',
+                price: {
+                    currencyCode: 'EUR',
+                    centAmount: 1234
+                }
+            }]
+        };
+
+        var objWithAmounts = {
+            name: 'Jessica',
+            surname: 'Simpson',
+            cartItems: [{
+                name: 'Book',
+                price: {
+                    currencyCode: 'EUR',
+                    amount: 12.34
+                }
+            }]
+        };
+
+        function clone(obj){
+            return JSON.parse(JSON.stringify(obj));
+        }
+
+        it('centAmountsToAmounts should convert centAmounts to amounts', function () {
+            var input = clone(objWithCentAmounts);
+            var output = clone(objWithAmounts);
+
+            var result = helpers.centAmountsToAmounts(input);
+            expect(result).toEqual(output);
+        });
+
+        it('amountsToCentAmounts should convert amounts to centAmounts', function () {
+            var input = clone(objWithAmounts);
+            var output = clone(objWithCentAmounts);
+
+            var result = helpers.amountsToCentAmounts(input);
+            expect(result).toEqual(output);
+        });
+
+    });
+
+
 });
