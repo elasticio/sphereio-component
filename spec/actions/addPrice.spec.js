bf607d5ed307890883cbad75772e0be7e9fabc44
Nenad <nenad@elastic.io>
2015-05-21 21:38:24 +0200

    Cover more

--- spec/actions/addPrice.spec.js
+++ spec/actions/addPrice.spec.js
@@ -183,7 +183,68 @@ describe('Add Price', function () {
         });
 
     });
+    describe('Price in scope exists in masterVariant should execute changePrice', function () {
+        var msg;
+        var self;
 
+        beforeEach(function () {
+            msg = {
+                body: {
+                    productId: 'f499d66d-5bb7-48b0-b2ee-891317abfd69',
+                    variantId: 1,
+                    amount: 100,
+                    currency: 'EUR'
+                }
+            };
+
+            self = jasmine.createSpyObj('self', ['emit']);
+
+            nock('https://api.sphere.io')
+                .get('/test_project/products/f499d66d-5bb7-48b0-b2ee-891317abfd69')
+                .reply(200, masterProduct.results[0])
+                .post('/test_project/products/f499d66d-5bb7-48b0-b2ee-891317abfd69', {
+                    'version': 12,
+                    'actions': [{
+                        'action': 'changePrice',
+                        'variantId': 1,
+                        'price': {
+                            'value': {
+                                'currencyCode': 'EUR',
+                                'centAmount': 10000
+                            }
+                        }
+                    }]
+                })
+                .reply(200, {
+                    version: 13
+                });
+
+            runs(function () {
+                addPrice.process.call(self, msg, cfg);
+            });
+
+            waitsFor(function () {
+                return self.emit.calls.length;
+            });
+        });
+
+        it('and call emit only 2 times', function () {
+            expect(self.emit.calls.length).toEqual(2);
+        });
+
+        it('and emit proper data message', function () {
+            var event = self.emit.calls[0].args[0];
+            var data = self.emit.calls[0].args[1].body;
+            expect(event).toEqual('data');
+            expect(data.version).toEqual(13);
+        });
+
+        it('and emit end message', function () {
+            var args = self.emit.calls[1].args;
+            expect(args[0]).toEqual('end');
+        });
+
+    });
     describe('general error', function () {
         var msg;
         var self;
