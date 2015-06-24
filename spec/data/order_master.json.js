bf6b891a562cf8b1959c93ead2a5120e155a6cfd
sdwvit <viktor.musiienko@elastic.io>
2015-06-24 20:21:50 +0300

    New solution for order identification number resolving Modified tests as well

--- spec/data/order_master.json.js
+++ spec/data/order_master.json.js
@@ -1,117 +1,236 @@
 module.exports = {
-  "offset": 0,
-  "count": 1,
-  "total": 1,
-  "results": [
-      {
-          "type":"Order",
-          "id":"8fd9f83c-3453-418c-9f3b-5a218bfc842a",
-          "version":12,
-          "orderNumber":null,
-          "createdAt":"2013-06-04T14:05:13.564Z",
-          "lastModifiedAt":"2015-06-03T14:37:43.191Z",
-          "orderState":"Open",
-          "totalPrice":{
-            "currencyCode":"EUR",
-            "centAmount":10200
-          },
-          "taxedPrice":{
-            "totalNet":{
-              "currencyCode":"EUR",
-              "centAmount":8262
+    "offset": 0,
+    "count": 1,
+    "total": 1,
+    "results": [
+        {
+            "type": "Order",
+            "id": "8fd9f83c-3453-418c-9f3b-5a218bfc842a",
+            "version": 11,
+            "orderNumber": "335",
+            "customerEmail": "bedylowoja@yahoo.com",
+            "createdAt": "2015-05-10T16:31:26.529Z",
+            "lastModifiedAt": "2015-06-23T15:19:15.702Z",
+            "orderState": "Open",
+            "totalPrice": {
+                "currencyCode": "EUR",
+                "centAmount": 10000
             },
-            "totalGross":{
-              "currencyCode":"EUR",
-              "centAmount":10200
-            },
-            "taxPortions":[
-              {
-                "rate":0.19,
-                "amount":{
-                  "currencyCode":"EUR",
-                  "centAmount":1938
-                }
-              }
-            ]
-          },
-          "lineItems":[
-            {
-              "id":"910baeb9-cddc-4723-a921-5770ed6f474e",
-              "productId":"70bd556e-21f8-45da-8742-a0b33e0f0fa7",
-              "name":{
-                "en":"GIRLS HARTBREAK CREW"
-              },
-              "productSlug":{
-                "en":"girls-hartbreak-crew1368609991619"
-              },
-              "variant":{
-                "id":1,
-                "sku":"sku_GIRLS_HARTBREAK_CREW_variant1_1368609991619",
-                "prices":[
-                  {
-                    "value":{
-                      "currencyCode":"EUR",
-                      "centAmount":3400
+            "taxedPrice": {
+                "totalNet": {
+                    "currencyCode": "EUR",
+                    "centAmount": 8403
+                },
+                "totalGross": {
+                    "currencyCode": "EUR",
+                    "centAmount": 10000
+                },
+                "taxPortions": [
+                    {
+                        "rate": 0.19,
+                        "amount": {
+                            "currencyCode": "EUR",
+                            "centAmount": 1597
+                        }
                     }
-                  }
-                ],
-                "images":[
-                  {
-                    "url":"https://sphere.io/cli/data/253234387_1.jpg",
-                    "dimensions":{
-                      "w":1400,
-                      "h":1400
-                    }
-                  }
-                ],
-                "attributes":[
-
                 ]
-              },
-              "price":{
-                "value":{
-                  "currencyCode":"EUR",
-                  "centAmount":3400
-                }
-              },
-              "quantity":3,
-              "taxRate":{
-                "name":"19% MwSt",
-                "amount":0.19,
-                "includedInPrice":true,
-                "country":"DE",
-                "id":"woNXPp-l"
-              },
-              "state":[
+            },
+            "country": "DE",
+            "lineItems": [
                 {
-                  "quantity":3,
-                  "state":{
-                    "typeId":"state",
-                    "id":"de672b0b-c9a3-4dc1-8d8d-298cd8d45fe1"
-                  }
+                    "id": "dd014a36-a59c-42c7-9b25-a299a94a73a8",
+                    "productId": "3326766a-3efe-4a28-8215-2964245ec224",
+                    "name": {
+                        "en": "testproduct for API Tests (don't delete)"
+                    },
+                    "productSlug": {
+                        "en": "testproduct-for-api-tests-dont-delete-1428739352968"
+                    },
+                    "variant": {
+                        "id": 1,
+                        "sku": "sku_testproduct_variant_142727402112312",
+                        "prices": [
+                            {
+                                "value": {
+                                    "currencyCode": "EUR",
+                                    "centAmount": 10000
+                                }
+                            },
+                            {
+                                "value": {
+                                    "currencyCode": "USD",
+                                    "centAmount": 12000
+                                }
+                            }
+                        ],
+                        "images": [
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/product_heroImage_1-Z_60ArJi.jpg",
+                                "dimensions": {
+                                    "w": 1280,
+                                    "h": 566
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/product_heroImage_2-UjUBWLJ6.jpg",
+                                "dimensions": {
+                                    "w": 1280,
+                                    "h": 566
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/product_heroImage_3-TsEBVN88.jpg",
+                                "dimensions": {
+                                    "w": 1280,
+                                    "h": 566
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/product_heroImage_4-ZJven_Oq.jpg",
+                                "dimensions": {
+                                    "w": 1280,
+                                    "h": 566
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/product_heroImage_5-4oa0VY0B.jpg",
+                                "dimensions": {
+                                    "w": 1280,
+                                    "h": 566
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/collection_image_1-eOVQfHc8.jpg",
+                                "dimensions": {
+                                    "w": 307,
+                                    "h": 380
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/collection_image_2-OuXJtH8P.jpg",
+                                "dimensions": {
+                                    "w": 146,
+                                    "h": 181
+                                }
+                            },
+                            {
+                                "url": "https://f1d55f96c0e92f313068-e6aef75e71b3ce81d7805c07f951661e.ssl.cf3.rackcdn.com/collection_image_3-ebtPmdeG.jpg",
+                                "dimensions": {
+                                    "w": 146,
+                                    "h": 181
+                                }
+                            }
+                        ],
+                        "attributes": [
+                            {
+                                "name": "color",
+                                "value": "black"
+                            },
+                            {
+                                "name": "size",
+                                "value": {
+                                    "key": "M",
+                                    "label": "M"
+                                }
+                            },
+                            {
+                                "name": "fabrics",
+                                "value": "98 % Merinowolle, 2 % Elastan"
+                            }
+                        ]
+                    },
+                    "price": {
+                        "value": {
+                            "currencyCode": "EUR",
+                            "centAmount": 10000
+                        }
+                    },
+                    "quantity": 1,
+                    "taxRate": {
+                        "name": "19% MwSt",
+                        "amount": 0.19,
+                        "includedInPrice": true,
+                        "country": "DE",
+                        "id": "BdHgnVWT"
+                    },
+                    "state": [
+                        {
+                            "quantity": 1,
+                            "state": {
+                                "typeId": "state",
+                                "id": "ba142cf0-3f8c-4669-b083-0decde7a3629"
+                            }
+                        }
+                    ]
                 }
-              ]
-            }
-          ],
-          "shippingAddress":{
-            "firstName":"Igor",
-            "lastName":"Drobiazko",
-            "country":"DE"
-          },
-          "customLineItems":[
-
-          ],
-          "transactionFee":false,
-          "syncInfo":[
-
-          ],
-          "returnInfo":[
-
-          ],
-          "discountCodes":[
-
-          ],
-          "lastMessageSequenceNumber":0
+            ],
+            "shippingAddress": {
+                "title": "Dr.",
+                "firstName": "James",
+                "lastName": "Bond",
+                "streetName": "Macy Farmer",
+                "postalCode": "71153",
+                "city": "Harum tempora voluptatem Laboris tempora occaecat eius laboriosam debitis quas distinctio In eum amet vel culpa",
+                "region": "Velit commodi voluptate sint eiusmod consectetur esse voluptate exercitation sed tempor velit illo veritatis doloribus in a mollit odit ut",
+                "country": "DE",
+                "company": "Mathews and Roy LLC",
+                "phone": "+325-39-1652229",
+                "email": "bedylowoja@yahoo.com",
+                "additionalAddressInfo": "Doloribus atque culpa similique sit accusantium et error accusantium enim eum blanditiis adipisci ut ut ullamco architecto porro"
+            },
+            "billingAddress": {
+                "title": "Dr.",
+                "firstName": "James",
+                "lastName": "Bond",
+                "streetName": "Macy Farmer",
+                "postalCode": "71153",
+                "city": "Harum tempora voluptatem Laboris tempora occaecat eius laboriosam debitis quas distinctio In eum amet vel culpa",
+                "region": "Velit commodi voluptate sint eiusmod consectetur esse voluptate exercitation sed tempor velit illo veritatis doloribus in a mollit odit ut",
+                "country": "DE",
+                "company": "Mathews and Roy LLC",
+                "phone": "+325-39-1652229",
+                "email": "bedylowoja@yahoo.com",
+                "additionalAddressInfo": "Doloribus atque culpa similique sit accusantium et error accusantium enim eum blanditiis adipisci ut ut ullamco architecto porro"
+            },
+            "shippingInfo": {
+                "shippingMethodName": "Express Delivery",
+                "price": {
+                    "currencyCode": "EUR",
+                    "centAmount": 0
+                },
+                "shippingRate": {
+                    "price": {
+                        "currencyCode": "EUR",
+                        "centAmount": 2500
+                    },
+                    "freeAbove": {
+                        "currencyCode": "EUR",
+                        "centAmount": 0
+                    }
+                },
+                "taxRate": {
+                    "name": "19% MwSt",
+                    "amount": 0.19,
+                    "includedInPrice": true,
+                    "country": "DE",
+                    "id": "BdHgnVWT"
+                },
+                "taxCategory": {
+                    "typeId": "tax-category",
+                    "id": "bac1ac57-0b21-49f6-8cb0-062088476053"
+                },
+                "deliveries": [],
+                "shippingMethod": {
+                    "typeId": "shipping-method",
+                    "id": "01f8095d-3273-4e6e-80b2-be166b903521"
+                }
+            },
+            "customLineItems": [],
+            "transactionFee": true,
+            "syncInfo": [],
+            "returnInfo": [],
+            "discountCodes": [],
+            "lastMessageSequenceNumber": 0
         }
     ]
-};
+}
