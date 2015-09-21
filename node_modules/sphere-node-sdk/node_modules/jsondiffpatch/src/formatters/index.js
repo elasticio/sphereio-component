4c08265ba348040715df14d9257910406bee744c
Igor Drobiazko <igor@elastic.io>
2015-09-21 08:50:47 +0200

    Removed old SDK

--- node_modules/sphere-node-sdk/node_modules/jsondiffpatch/src/formatters/index.js
+++
@@ -1,9 +0,0 @@
-var environment = require('../environment');
-
-exports.html = require('./html');
-exports.annotated = require('./annotated');
-
-if (!environment.isBrowser) {
-	var consoleModuleName = './console';
-	exports.console = require(consoleModuleName);
-}
